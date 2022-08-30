47f64ef8ec0a8de7526687cf8755bda0e80cacb9
EliezerAmbion <eliezerambion@gmail.com>
2022-08-30 14:46:30 +0800

    add rest api

---
+++ src/routers/user.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const router = new express.Router();
+const User = require('../models/user');
+
+router.post('/users', async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.get('/users/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findById(_id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.patch('/users/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const updates = Object.keys(req.body);
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid Updates' });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+router.delete('/users/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findByIdAndDelete(_id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;
